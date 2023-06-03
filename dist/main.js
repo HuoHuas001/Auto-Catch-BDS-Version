fdcfc1a70964a3a82a9e31beb57a5321ff18a0c5
HuoHuas001 <2351078777@qq.com>
2023-06-04 01:14:11 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -34,7 +34,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+//Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const request_1 = __importDefault(require("request"));
 function getLINK(url) {
