bb9dad5f378ff1dfb12bd0d33eca6b9b52a537c2
HuoHuas001 <2351078777@qq.com>
2023-06-04 01:33:55 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
 import * as core from '@actions/core'
-import request from 'request';
+import request from 'request'
 
-async function getLINK(url:string) {
-    let data = "";
-    await request.get({ url: url }, (err, httpResponse, body) => {
-        if (err) {
-            return ""
-        }
-        data = body;
-    });
-    return data
+function getLINK(url: string): string {
+  let data = ''
+  request.get({url}, (err, httpResponse, body) => {
+    if (err) {
+      return ''
+    }
+    data = body
+  })
+  return data
 }
 
-
-
 async function run(): Promise<void> {
-    //const url: string = core.getInput('url')
-    let url:string = await getLINK('https://raw.githubusercontent.com/LiteLDev/LteLoaderBDS/develop/scripts/LINK.txt')
-    
-    let fileName: string = url.split("-").at(-1)||"1.19.81.01";
-    let BDSVersion = fileName.replace(".zip","")
-    core.setOutput("BDS_VERSION", BDSVersion);
+  //const url: string = core.getInput('url')
+  const url: string = getLINK(
+    'https://raw.githubusercontent.com/LiteLDev/LteLoaderBDS/develop/scripts/LINK.txt'
+  )
+
+  const fileName: string = url.split('-').at(-1) || '1.19.81.01'
+  const BDSVersion: string = fileName.replace('.zip', '')
+  core.setOutput('BDS_VERSION', BDSVersion)
 }
 
 run()
