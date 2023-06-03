e2e485c7d184d2a79d0517fb5d58791cf279ea97
HuoHuas001 <2351078777@qq.com>
2023-06-04 00:09:35 +0800

    fix problem

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import * as core from '@actions/core'
 
 async function run(): Promise<void> {
-
     const url: string = core.getInput('url')
-    let fileName = url.split("-").at(-1);
+    let fileName: string = url.split("-").at(-1)||"1.19.81.01";
     let BDSVersion = fileName.replace(".zip","")
     core.setOutput("BDS_VERSION", BDSVersion);
-    
 }
 
 run()
